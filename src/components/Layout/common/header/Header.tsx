2fe77fb355021439034de5530a2e663c11754800
Mohamed Khairy <Mohamedbakr225@gmail.com>
2025-04-12 10:45:05 +0200

    done changed api

--- src/components/Layout/common/header/Header.tsx
+++ src/components/Layout/common/header/Header.tsx
@@ -120,7 +120,7 @@ const Header: FC<IProps> = () => {
                                     <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5">
                                         <button
                                             onClick={signOut}
-                                            className="block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                                            className="block px-4 py-2 text-sm text-gray-700 cursor-pointer w-full text-left hover:bg-gray-100"
                                         >
                                             Sign out
                                         </button>
